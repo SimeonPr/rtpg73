2193f832073a6506f8d256ede993317a6fa3383e
Simeon Prause <simeon.prause@icloud.com>
2025-01-23 21:48:30 +0100

    fsm.rs: A bit of Cleanup + limiting the interface.

--- Exercise 3/src/fsm.rs
+++ Exercise 3/src/fsm.rs
@@ -3,19 +3,19 @@ use driver_rust::elevio::elev::Elevator;
 use crate::timer::{self, Timer};
 
 #[derive(Debug)]
-pub enum ElevatorBehaviour {
+enum ElevatorBehaviour {
     Idle,
     DoorOpen,
     Moving
 }
 #[derive(Debug, Copy, Clone)]
-pub enum Dirn {
+enum Dirn {
     Down = -1,
     Stop = 0,
     Up = 1
 }
 #[derive(Debug)]
-pub enum Button {
+enum Button {
     HallUp,
     HallDown,
     Cab
@@ -34,12 +34,25 @@ struct DirectionBehaviourPair {
     behavior: ElevatorBehaviour
 }
 impl ElevatorState {
+    
+    pub fn init_elevator(elevator_connection: Elevator) -> ElevatorState {
+        ElevatorState {
+            floor: -1,
+            dirn: Dirn::Stop,
+            requests: [[0;3]; 4],
+            behaviour: ElevatorBehaviour::Idle,
+            door_open_duration: 3.0,
+            connection: elevator_connection
+        }
+    }
+    
     pub fn fsm_on_init_between_floors(&mut self) {
         //motor direction
         self.connection.motor_direction(Dirn::Down as u8);
         self.dirn = Dirn::Down;
         self.behaviour = ElevatorBehaviour::Moving;
     }
+    
     pub fn fsm_on_request_button_press(&mut self, floor: i8, call: u8, timer: &mut timer::Timer) {
         match self.behaviour {
             ElevatorBehaviour::DoorOpen => {
@@ -73,6 +86,57 @@ impl ElevatorState {
         
         self.set_all_lights();
     }
+
+    pub fn fsm_on_door_time_out(&mut self, timer: &mut Timer) {
+        match self.behaviour {
+            ElevatorBehaviour::DoorOpen => {
+                let pair: DirectionBehaviourPair = self.requests_choose_direction();
+                self.dirn = pair.dirn;
+                self.behaviour = pair.behavior;
+
+                match self.behaviour {
+                    ElevatorBehaviour::DoorOpen => {
+                        timer.start(self.door_open_duration);
+                        self.requests_clear_at_current_floor();
+                        self.set_all_lights();
+                    },
+                    ElevatorBehaviour::Moving | ElevatorBehaviour::Idle => {
+                        self.connection.door_light(false);
+                        self.connection.motor_direction(self.dirn as u8);
+                    }
+                }
+            },
+            _ => {}
+        }
+    }
+    
+    pub fn fsm_on_floor_arrival(&mut self, floor: i8, timer: &mut timer::Timer) {
+        self.floor = floor;
+        self.connection.floor_indicator(self.floor as u8);
+
+        match self.behaviour {
+            ElevatorBehaviour::Moving => {
+                if self.requests_should_stop() {
+                    self.connection.motor_direction(Dirn::Stop as u8);
+                    self.connection.door_light(true);
+                    self.requests_clear_at_current_floor();
+                    // timer
+                    timer.start(self.door_open_duration);
+                    //self.set_all_light();
+                    self.behaviour = ElevatorBehaviour::DoorOpen;
+                }
+            }
+            _ => {},
+        };
+        self.connection.motor_direction(Dirn::Stop as u8);
+    }
+
+    pub fn fsm_on_stop_button_press(&mut self){}
+
+    fn requests_should_clear_immediately(&mut self, floor: i8, _call: u8) -> bool {
+         self.floor == floor
+    }
+    
     fn set_all_lights(&self) {
         for f in 0..4 {
             for b in 0..3 {
@@ -80,6 +144,7 @@ impl ElevatorState {
             }
         }
     }
+    
     fn requests_choose_direction(&mut self) -> DirectionBehaviourPair {
         match self.dirn {
             Dirn::Up => {
@@ -117,57 +182,13 @@ impl ElevatorState {
             }
         }
     }
-    pub fn fsm_on_door_time_out(&mut self, timer: &mut Timer) {
-        match self.behaviour {
-            ElevatorBehaviour::DoorOpen => {
-                let pair: DirectionBehaviourPair = self.requests_choose_direction();
-                self.dirn = pair.dirn;
-                self.behaviour = pair.behavior;
-
-                match self.behaviour {
-                    ElevatorBehaviour::DoorOpen => {
-                        timer.start(self.door_open_duration);
-                        self.requests_clear_at_current_floor();
-                        self.set_all_lights();
-                    },
-                    ElevatorBehaviour::Moving | ElevatorBehaviour::Idle => {
-                        self.connection.door_light(false);
-                        self.connection.motor_direction(self.dirn as u8);
-                    }
-                }
-            },
-            _ => {}
-        }
-    }
-    fn requests_should_clear_immediately(&mut self, floor: i8, _call: u8) -> bool {
-         self.floor == floor
-    }
     
-    pub fn fsm_on_floor_arrival(&mut self, floor: i8, timer: &mut timer::Timer) {
-        self.floor = floor;
-        self.connection.floor_indicator(self.floor as u8);
-
-        match self.behaviour {
-            ElevatorBehaviour::Moving => {
-                if self.requests_should_stop() {
-                    self.connection.motor_direction(Dirn::Stop as u8);
-                    self.connection.door_light(true);
-                    self.requests_clear_at_current_floor();
-                    // timer
-                    timer.start(self.door_open_duration);
-                    //self.set_all_light();
-                    self.behaviour = ElevatorBehaviour::DoorOpen;
-                }
-            }
-            _ => {},
-        };
-        self.connection.motor_direction(Dirn::Stop as u8);
-    }
     fn requests_clear_at_current_floor(&mut self) {
         for b in 0..3 {
             self.requests[self.floor as usize][b as usize] = 0;
         }
     }
+    
     fn requests_here(&self) -> bool {
         for b in 0..3 {
             if self.requests[self.floor as usize][b as usize] == 1 {
@@ -176,6 +197,7 @@ impl ElevatorState {
         }
         return false;
     }
+    
     fn requests_below(&self) -> bool {
         for f in 0..self.floor {
             for b in 0..3 {
@@ -186,6 +208,7 @@ impl ElevatorState {
         }
         return false;
     }
+    
     fn requests_above(&self) -> bool {
         for f in self.floor..4 {
             for b in 0..3 {
@@ -196,6 +219,7 @@ impl ElevatorState {
         }
         return false;
     }
+    
     fn requests_should_stop(&self) -> bool {
         match self.dirn {
             Dirn::Down => {
@@ -211,16 +235,5 @@ impl ElevatorState {
             Dirn::Stop => {true}
         }
     }
-    pub fn fsm_on_stop_button_press(&mut self){}
-
-    pub fn init_elevator(elevator_connection: Elevator) -> ElevatorState {
-        ElevatorState {
-            floor: -1,
-            dirn: Dirn::Stop,
-            requests: [[0;3]; 4],
-            behaviour: ElevatorBehaviour::Idle,
-            door_open_duration: 3.0,
-            connection: elevator_connection
-        }
-    }
+    
 }
