c4dcf5a86c866a526db26f7edd116336c198ab8e
timoabr <157982222+timoabr@users.noreply.github.com>
2025-03-26 09:58:24 +0100

    fixed the motor stopp error, but should test

--- main/src/manager.rs
+++ main/src/manager.rs
@@ -381,7 +381,7 @@ impl WorldView {
         for (id, elev) in self.elevators.iter() {
             if 
             (*id != self.id) && 
-            (elev.last_received.elapsed().expect("elapsed() failed") > Duration::from_secs(timeout)) ||
+            ((elev.last_received.elapsed().expect("elapsed() failed")) > Duration::from_secs(timeout)) ||
             (!elev.is_working)
             {continue;}
             alive_elevators.insert(*id);
